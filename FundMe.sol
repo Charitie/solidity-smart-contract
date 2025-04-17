b2d7a67ad5417d602c02b33c9127756a27fb4aeb
Charitie <charitytarus21@gmail.com>
2025-04-17 12:30:41 +0300

    fund account

--- FundMe.sol
+++ FundMe.sol
@@ -1,37 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts@1.3.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
 
-    uint256 public minUsd = 5;
+    uint256 public minUsd = 5e18;
+
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public  addressToAmountFunded;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minUsd, "Eth sent is too low" );    
+        require(msg.value.getConversionRate() >= minUsd, "Eth sent is too low" );   
+        funders.push(msg.sender); 
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns{
-       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
-       ( /* uint80 roundId */,
-            int256 answer,
-            /*uint256 startedAt*/,
-            /*uint256 updatedAt*/,
-            /*uint80 answeredInRound*/) = priceFeed.latestRoundData()
-
-            return uint256 answer * 1e10
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {  
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
-        return ethAmountInUsd
-    }
-
-
-    function getVersion() public view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
+    // function getVersion() public view returns (uint256) {
+    //     return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    // }
 
 }
---
+++ PriceConverter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts@1.3.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    
+   function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 answer,,,) = priceFeed.latestRoundData();
+
+        return uint256(answer) * 1e10;
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {  
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
+        return ethAmountInUsd;
+    }
+}
